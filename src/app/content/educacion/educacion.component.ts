3db4d3f0dabe4fe28e89d3ffc94bde039fbf214f
Jank1510 <89560208+Jank1510@users.noreply.github.com>
2022-09-15 21:55:26 -0500

    update

--- src/app/content/educacion/educacion.component.ts
+++ src/app/content/educacion/educacion.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./educacion.component.css']
 })
 export class EducacionComponent implements OnInit {
+  obecjtEstudios: any
 
-  constructor() { }
+  constructor() {
+    this.obecjtEstudios =[ { 
+    titulo: "Programación con énfasis en aplicaciones web",
+    lugar:"Universidad Autónoma de Bucaramanga-UNAB",
+    imagen:"https://misiontic2022.unab.edu.co/pluginfile.php/1/theme_academi/logo/-1/LogoUnab-Horizontal.png",
+    linkPreview:"https://drive.google.com/file/d/1azE_1-bS-DaQ-6vil9r52fwF1UNsvUdl/view?usp=sharing"
+
+     }, {}]
+  }
 
   ngOnInit(): void {
   }
-
 }
--- src/app/content/inicio/inicio.component.ts
+++ src/app/content/inicio/inicio.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
